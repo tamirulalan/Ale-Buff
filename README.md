0b3b2af37d97a92606d6110ffe2e0bd56a2435ea
tamirulalan <tamirulalan@gmail.com>
2022-11-17 21:20:36 -0500

    Read me file, CSS, HTML and js file were finalised

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# The game title was changed from The-battle-between-me-and-my-flesh to Ale-Buff
-//i am planning to develop game based on Gel 5:19-22
+Project-1
+
+Alle-Buff 
+
+=====How to play====
+
+This game will be played between two different players. In this case between me and computer. Before we start the game both players save two numbers less than 5. The numbers must be different (say x for user and y for computer). Then we have five fingers representing numbers from one to five. The user player('me' in this case) can choose these numbers by clicking on any fingers he/she wants. In the same way the computer select randomly one number from one to five. After this both numbers added together and check who win the game. If the sum nymber is equal with "x" which is my number I (the user) will win the game and my score goes up. But if the sum number is equal with "y" which is computer number the computer win the game AND computer score goes up. Other wise you will draw and no score change. 
+
+For this game the maximum attempt for one time game is 20
+
+how to access it (fork and clone, etc.)
+Link to the GitHub page (live site for game)
+Technologies used
+=======//========
+I used HTML CSS and javascript
+
+
+Any acknowledgements
+========////=======
+I highly acknowlegde my Instructor Tishana for her guidance, help and giving me ontime direction to finish this project. I also acknowlegde my friend Gary
+ 
+ 
+ Resources 
+ ======///=====
+ Youtube, books and articles
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// initializing Variables====
 let myscore =0
 let compscore =0
+let attempt =1
 
+   //getting DOM from HTML=====
 let displayScore =document.getElementById('displayScore')
 let myScore =document.getElementById('myScore')
 let compScore =document.querySelector('#compScore')
@@ -8,16 +11,10 @@ let rondomChoice =document.getElementById('rondomChoice')
 let myFirstChoice =document.getElementById('myFirstChoice')
 let message =document.getElementById('message')
 let mychoice =document.querySelectorAll('.myChoice')
-let oneFinger =document.getElementById('one')
-let twoFinger =document.getElementById('two')
-let threeFinger =document.getElementById('three')
-let fourFinger =document.getElementById('four')
-let fiveFinger =document.getElementById('five')
 let user=document.getElementById('user').value
 let comp =document.getElementById('comp')
 let btn =document.getElementById('btn')
 let reserbtn =document.getElementById('resetbtn')
-//let userInput =document.getElementById('userInput')
 let attempts =document.getElementById("attempts")
 const modal = document.querySelector('#my-modal');
 const modalBtn = document.querySelector('#modal-btn');
@@ -29,68 +26,38 @@ let name=document.getElementById('Name')
 let animation =document.getElementById('animation')
 let tk =document.getElementById('tk')
 let gud =document.querySelector('.gud')
-let attempt =1
 
-function getCompNumber (){
-    //const choices= ['one', 'two', 'three', 'four', 'five']
-    const compNumber = (Math.floor(Math.random()*5))
-    //return choices[compNumber]
-}
+
+// ==== get the first ramdom number===
 const compFirstNumber = (Math.floor(Math.random()*5))
 
 //======modal====
-       
+let input =userInput.value
 modalBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', outsideClick);
 footerbtn.addEventListener('click', ()=>{
  modal.style.display = 'none';
  modalBtn.style.display = 'none';
  intro.style.display = 'none';
  gud.style.display='none'
  container.style.display = 'block'
- //let usernumber =userInput.value
  myFirstChoice.textContent =userInput.value
- const compFirstNumber=(Math.floor(Math.random()*5))
  rondomChoice.textContent=compFirstNumber
 })
 
-console.log(compFirstNumber)
-// Open
+
+//===== Open modal function===
 function openModal() {
   modal.style.display = 'block';
 }
 
-// Close
+// =====Close button function====
 function closeModal() {
   modal.style.display = 'none';
 }
 
-// Close If Outside Click
-function outsideClick(e) {
-  if (e.target == modal) {
-    modal.style.display = 'none';
-  }
-}
-
-
-
-//===game function====
-
-function game(para){
-    const compNumber = (Math.floor(Math.random()*5))
-    //const compFirstNumber=(Math.floor(Math.random()*5))
-    let input =userInput.value
-    
-    attempts.innerHTML=attempt
-    
- 
-    console.log(compNumber)
-    
-    console.log(input)
- if(attempt<20){
-    attempt ++
-    if (compNumber + para ==input){
+//=====winning function====
+function win(){
             myscore++
             myScore.innerText =myscore
             message.innerHTML=  "You Won the game!! 🔥"
@@ -105,9 +72,11 @@ function game(para){
           }
           
           setInterval(changeColor,100);
-        
-        } else if (compNumber + para ==compFirstNumber){
-            compscore++
+}
+
+//=====loosing function=====
+function loose(){
+  compscore++
             compScore.innerText =compscore
             message.innerHTML=  "computer Won the game!!"
             function changeScr() {
@@ -121,46 +90,77 @@ function game(para){
               }
               
               setInterval(changeColor,100);
-        }else{
-            message.innerHTML ="draw"
-            function changeScr() {
-                document.getElementById("tk").src='images/Draw.gif';
-              }
-              changeScr()
-           
-        }
 
- }else {
-    message.innerHTML ="Gave Over"
-    message.style.color='red'
-    if(myscore>compscore){
-      window.alert(`You won computer by  ${myscore} : ${compscore}`);
-    }else if(myscore < compscore){
-      window.alert(`computer won You by  ${compscore}:${myscore}`);
-    } else if (myscore = compscore){
-      window.alert(`You scored the same ${myscore} : ${compscore}`);
-    } else{
-      window.alert(`you can play agaion`)
-    }
-    
-    let btnReset = document.createElement("button");
-    btnReset.setAttribute("class",'btnReset')
-    btnReset.innerHTML = "Reset";
-    animation.appendChild(btnReset);
-    tk.innerHTML=btnReset
-    tk.style.display ='none'
-    btnReset.addEventListener('click', ()=>{
-        window.location.reload();
-    })
- }
 }
+
+// ===draw function====
+function draw(){
+  message.innerHTML ="draw"
+  function changeScr() {
+      document.getElementById("tk").src='images/Draw.gif';
+    }
+    changeScr()
    
+}
+//===game function====
+
+function game(para){
+    const compNumber = (Math.floor(Math.random()*5))
+    let input =userInput.value
+    attempts.innerHTML=attempt
     
+  if(attempt<20){
+      attempt ++
+      if (compNumber + para ==input){
+        win()
+      } else if (compNumber + para ==compFirstNumber){
+          loose()
+      }else{
+            draw()  
+      }
+  }else {
+      message.innerHTML ="Gave Over"
+      message.style.color='red'
+      if(myscore>compscore){
+        window.alert(`You won computer by  ${myscore} : ${compscore}`);
+      }else if(myscore < compscore){
+        window.alert(`computer won You by  ${compscore}:${myscore}`);
+      } else if (myscore = compscore){
+        window.alert(`You scored the same ${myscore} : ${compscore}`);
+      } else{
+        window.alert(`you can play agaion`)
+      }
+      
+      let btnReset = document.createElement("button");
+      btnReset.setAttribute("class",'btnReset')
+      btnReset.innerHTML = "Reset";
+      animation.appendChild(btnReset);
+      tk.innerHTML=btnReset
+      tk.style.display ='none'
+      btnReset.addEventListener('click', ()=>{
+          window.location.reload();
+      })
+  }
+  }
+    
+  //=====starting the game====
     function main(){
     mychoice.forEach(function(myChoice) {
       myChoice.addEventListener('click', (e)=>{
-        game(e.target.value)
+        const stlylist =e.currentTarget.classList
+        if(stlylist.contains('one')){
+          game(1)
+        }else if (stlylist.contains('two')){
+          game(2)
+        }else if (stlylist.contains('three')){
+          game(3)
+        }else if (stlylist.contains('four')){
+          game(4)
+        }else {
+          game(5)
+        }
       })
     });
-     }
+  }
     main()
+    
